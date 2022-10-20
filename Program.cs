43d796a2425eeacdab7f734aea3f550e1f1dd4c6
Emre Basarir <yunu_emre_kardes@outlook.com>
2022-10-20 18:54:26 +0300

    enum

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sıcaklik = 32;
+
+            if(sıcaklik <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim");
+            else if(sıcaklik >= (int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            else if(sıcaklik >= (int)HavaDurumu.Normal && sıcaklik < (int)HavaDurumu.CokSıcak)
+                Console.WriteLine("Haydi dışarıya çıkalım!");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma = 23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+C# (Enum)[https://app.patika.dev/courses/csharp-101/17-enum]
